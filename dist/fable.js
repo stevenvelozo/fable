16eddeb33a20e1f0feb74810ab50bd2f6ec526df
Steven Velozo <steven@velozo.com>
2023-02-24 17:24:15 -0800

    New fable log version support with ... less pressure.

--- dist/fable.js
+++ dist/fable.js
@@ -62,6 +62,10 @@
           // We added this as the mechanism for tracking loggers to allow multiple simultaneous streams
           // to the same provider.
           this.loggerUUID = this.generateInsecureUUID();
+
+          // Eventually we can use this array to ompute which levels the provider allows.
+          // For now it's just used to precompute some string concatenations.
+          this.levels = ["trace", "debug", "info", "warn", "error", "fatal"];
         }
 
         // This is meant to generate programmatically insecure UUIDs to identify loggers
@@ -136,25 +140,43 @@
       class ConsoleLogger extends libBaseLogger {
         constructor(pLogStreamSettings, pFableLog) {
           super(pLogStreamSettings);
-          this._ShowTimeStamps = pLogStreamSettings.hasOwnProperty('ShowTimeStamps') ? pLogStreamSettings.ShowTimeStamps == true : false;
-          this._FormattedTimeStamps = pLogStreamSettings.hasOwnProperty('FormattedTimeStamps') ? pLogStreamSettings.FormattedTimeStamps == true : false;
-          this._ContextMessage = pLogStreamSettings.hasOwnProperty('Context') ? ` (${pLogStreamSettings.Context})` : pFableLog._Settings.hasOwnProperty('Product') ? ` (${pFableLog._Settings.Product})` : '';
+          this._ShowTimeStamps = pLogStreamSettings.hasOwnProperty('showtimestamps') ? pLogStreamSettings.showtimestamps == true : false;
+          this._FormattedTimeStamps = pLogStreamSettings.hasOwnProperty('formattedtimestamps') ? pLogStreamSettings.formattedtimestamps == true : false;
+          this._ContextMessage = pLogStreamSettings.hasOwnProperty('Context') ? `(${pLogStreamSettings.Context})` : pFableLog._Settings.hasOwnProperty('Product') ? `(${pFableLog._Settings.Product})` : 'Unnamed_Log_Context';
+
+          // Allow the user to decide what gets output to the console
+          this._OutputLogLinesToConsole = pLogStreamSettings.hasOwnProperty('outputloglinestoconsole') ? pLogStreamSettings.outputloglinestoconsole : true;
+          this._OutputObjectsToConsole = pLogStreamSettings.hasOwnProperty('outputobjectstoconsole') ? pLogStreamSettings.outputobjectstoconsole : true;
+
+          // Precompute the prefix for each level
+          this.prefixCache = {};
+          for (let i = 0; i <= this.levels.length; i++) {
+            this.prefixCache[this.levels[i]] = `[${this.levels[i]}] ${this._ContextMessage}: `;
+            if (this._ShowTimeStamps) {
+              // If there is a timestamp we need a to prepend space before the prefixcache string, since the timestamp comes first
+              this.prefixCache[this.levels[i]] = ' ' + this.prefixCache[this.levels[i]];
+            }
+          }
         }
         write(pLevel, pLogText, pObject) {
+          let tmpTimeStamp = '';
           if (this._ShowTimeStamps && this._FormattedTimeStamps) {
-            let tmpDate = new Date().toISOString();
-            console.log(`${tmpDate} [${pLevel}]${this._ContextMessage} ${pLogText}`);
+            tmpTimeStamp = new Date().toISOString();
           } else if (this._ShowTimeStamps) {
-            let tmpDate = +new Date();
-            console.log(`${tmpDate} [${pLevel}]${this._ContextMessage} ${pLogText}`);
-          } else {
-            console.log(`[${pLevel}]${this._ContextMessage} ${pLogText}`);
+            tmpTimeStamp = +new Date();
+          }
+          let tmpLogLine = `${tmpTimeStamp}${this.prefixCache[pLevel]}${pLogText}`;
+          if (this._OutputLogLinesToConsole) {
+            console.log(tmpLogLine);
           }
 
           // Write out the object on a separate line if it is passed in
-          if (typeof pObject !== 'undefined') {
-            console.log(JSON.stringify(pObject, null, 4));
+          if (this._OutputObjectsToConsole && typeof pObject !== 'undefined') {
+            console.log(JSON.stringify(pObject, null, 2));
           }
+
+          // Provide an easy way to be overridden and be consistent
+          return tmpLogLine;
         }
       }
       module.exports = ConsoleLogger;
@@ -181,7 +203,7 @@
         constructor(pFableSettings, pFable) {
           let tmpSettings = typeof pFableSettings === 'object' ? pFableSettings : {};
           this._Settings = tmpSettings;
-          this._Providers = require('./Fable-Log-DefaultProviders.js');
+          this._Providers = require('./Fable-Log-DefaultProviders-Node.js');
           this._StreamDefinitions = tmpSettings.hasOwnProperty('LogStreams') ? tmpSettings.LogStreams : require('./Fable-Log-DefaultStreams.json');
           this.logStreams = [];
 
@@ -343,7 +365,7 @@
         FableLog: FableLog
       };
     }, {
-      "./Fable-Log-DefaultProviders.js": 2,
+      "./Fable-Log-DefaultProviders-Node.js": 2,
       "./Fable-Log-DefaultStreams.json": 3
     }],
     6: [function (require, module, exports) {
