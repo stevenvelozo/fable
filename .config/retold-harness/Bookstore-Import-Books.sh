8c2e5b6245dbd1c8ea8c8574aa4d5f73aaec725f
Steven Velozo <steven@velozo.com>
2023-05-10 15:57:42 -0700

    Added data pipeline test harness

---
+++ .config/retold-harness/Bookstore-Import-Books.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+# Eventually this will delete the database too!
+node ./bookstore-import-books-run.js
--- .config/retold-harness/bookstore-import-books-run.js
+++ .config/retold-harness/bookstore-import-books-run.js
@@ -1,1 +1,9 @@
-let tmpRun = require('./bookstore-import-books.js')();
+let tmpRun = require('./bookstore-import-books.js')(
+    (pError)=>
+    {
+        if (pError)
+        {
+            console.log(`ERROR: ${pError}`, pError);
+        }
+        process.exit();
+    });
